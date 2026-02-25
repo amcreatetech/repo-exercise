c57f070c21422f90693b316a51277a9992e14f08
madiwel <121457858+madiwel@users.noreply.github.com>
2026-02-26 00:08:34 +0300

    main

--- CarAm/controllers/contact_registration_controller.py
+++ CarAm/controllers/contact_registration_controller.py
@@ -104,8 +104,9 @@ class ContactRegistrationController(http.Controller):
                 'quantity': 1,
                 'price_unit': amount,
             })],
+
             'is_from_api': True,
-        })
+})
         
         # Post the credit note to make it effective
         credit_note.action_post()
--- CarAm/models/account_payment.py
+++ CarAm/models/account_payment.py
@@ -27,6 +27,7 @@ class AccountPayment(models.Model):
     caram_attachment_name = fields.Char(
         string="File Name"
     )
+
     is_from_api = fields.Boolean(
         string="Created from API",
         default=False,
@@ -34,6 +35,7 @@ class AccountPayment(models.Model):
         readonly=True,
         copy=False,
     )
+
 
     def _get_caram_api_url(self):
         """Get CarAm API base URL from settings"""
