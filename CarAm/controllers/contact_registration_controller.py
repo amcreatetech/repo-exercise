c1d2a300aa0dc470d8bdcca9ace8f49f9bbc1d9f
madiwel <121457858+fjahjah93@users.noreply.github.com>
2026-08-22 00:52:59 +0300

    add api/wallets/account-activites

--- CarAm/controllers/contact_registration_controller.py
+++ CarAm/controllers/contact_registration_controller.py
@@ -1,4 +1,7 @@
+from datetime import datetime
 import traceback
+
+from torch import ceil
 
 from odoo import fields, http, _
 from odoo.http import request
@@ -509,6 +512,83 @@ class ContactRegistrationController(http.Controller):
             {"error": str(e)},
             status=500,
         )    
+
+    @http.route('/api/wallets/account-activites', type='http', auth='none', methods=['GET'], csrf=False)
+    def get_wallet_transactions(self, user_id=None, company_id=None, from_date=None, to_date=None, sort_by=None, **kwargs):
+        company_id = company_id or request.env.user.company_id.id
+        try:
+            user = self._authenticate()
+
+            if not user_id:
+                return request.make_json_response({"error": "user_id is required"}, status=400)
+
+            env = self._get_env(user, company_id)
+
+            partner = env["res.partner"].sudo().search(
+                [("subID", "=", user_id)], limit=1
+            )
+            if not partner.exists():
+                return request.make_json_response({"error": "user_id not found"}, status=404)
+
+            card = env["loyalty.card"].sudo().search([("partner_id", "=", partner.id)], limit=1)
+            if not card:
+                return request.make_json_response({"error": "No Wallet found"}, status=404)
+
+
+            order = "id DESC" if sort_by == "desc" else "id ASC"
+            
+
+            # -------------------- Date filters --------------------
+            try:
+                from_date_parsed = datetime.strptime(from_date, "%Y-%m-%d").date() if from_date else None
+                to_date_parsed = datetime.strptime(to_date, "%Y-%m-%d").date() if to_date else None
+            except ValueError:
+                return request.make_json_response({"error": "Date format not valid"}, status=400)
+
+            history = env["loyalty.history"].sudo().search([("card_id", "=", card.id)])
+            if from_date_parsed:
+                history = history.filtered(lambda x: x.create_date.date() >= from_date_parsed)
+            if to_date_parsed:
+                history = history.filtered(lambda x: x.create_date.date() <= to_date_parsed)
+            history = history.filtered(
+                lambda x: x.issued > 0.0 or x.used > 0.0 or x.deposit_request > 0.0
+                or x.withdraw_request > 0.0 or x.cancelled_amount > 0.0
+            )
+
+            total_lines = len(history)
+            total_pages = ceil(total_lines) if total_lines else 0
+
+            page_history = env["loyalty.history"].sudo().search(
+                [("id", "in", history.ids)], order=order
+            )
+
+            data = [{
+                "description": str(line.description),
+                "transaction_date": str(line.create_date)[:16],
+                "deposit": "{:.2f}".format(line.issued) if line.issued else "",
+                "withdraw": "{:.2f}".format(line.used) if line.used else "",
+                "status": str(line.status) if line.status else "",
+            } for line in page_history]
+
+            return request.make_json_response(
+                {
+                    "status": "success",
+                    "message": "Wallet transactions fetched successfully",
+                    "data": {
+                        "user_id": user_id,
+                        "wallet_transactions": data,
+                        "pagination": {
+                            "current_page": page_number,
+                            "page_size": page_size,
+                            "total_activites": total_lines
+                        },
+                    },
+                },
+                status=200,
+            )
+
+        except Exception as e:
+            return request.make_json_response({"error": f"Failed to fetch wallet transactions: {str(e)}"}, status=500)
     
     def old_get_balance(self, **kw):
         """Return wallet balances from Loyality Card."""
