503bc884319a1e37c4c096fbb919c6cf5b6034f7
madiwel <121457858+fjahjah93@users.noreply.github.com>
2026-08-27 01:35:20 +0300

    Update contact_registration_controller.py

--- CarAm/controllers/contact_registration_controller.py
+++ CarAm/controllers/contact_registration_controller.py
@@ -1777,7 +1777,7 @@ class ContactRegistrationController(http.Controller):
                     company_id=ride.company_id.id,
                 )
 
-                ride.sudo().write({"has_wallet_entries": True,"status": "paid"})
+                ride.sudo().write({"has_wallet_entries": True,"state": "paid"})
 
 
                 if ride.coupon_value > 0:
